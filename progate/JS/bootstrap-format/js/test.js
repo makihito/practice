7f850e70aca1d538da60631bd990031345685bc9
akihito maeta <petelibertine200@gmail.com>
2019-09-01 13:36:35 +0900

    test

---
+++ progate/JS/bootstrap-format/js/test.js
@@ -0,0 +1,19 @@
+// 「Hello World」とコンソールに出力してください
+console.log("Hello World");
+
+// 「にんじゃわんこ」とコンソールに出力してください
+console.log("にんじゃわんこ");
+
+// 以下の行をコメントアウトしてください
+// console.log("この行をコメントアウトしてください");
+
+console.log(5 + 7);
+console.log(5 - 7);
+console.log("5+7");
+console.log(5 * 7);
+console.log(5 / 7);
+console.log(50 % 6);
+console.log("テスト" + "      " + "したい");
+
+let name = "忍者ワンっこ";
+console.log(name);
