87cdbbcd16ed5ec1709133ed7a798305dadd859c
akihito maeta <petelibertine200@gmail.com>
2019-09-03 23:25:32 +0900

    test

--- progate/jquery/js/test.js
+++ progate/jquery/js/test.js
@@ -1,5 +1,8 @@
 $(function() {
-    $('#hide-text').click(function() {
-        $('#text').slideUp(1000);
+    $('#change-text').click(function() {
+        $('#text').text('ようこそ、Progateへ');
+    })
+    $('#change-html').click(function() {
+        $('#text').html('<a href="https://prog-8.com/">ようこそ、Progateへ</a>');
     })
 })
