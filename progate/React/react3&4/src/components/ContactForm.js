26d561bdb7926b91151f5e018aed66b6e8f8bfb8
akihito maeta <petelibertine200@gmail.com>
2020-07-23 07:38:45 +0900

    desktop

--- progate/React/react3&4/src/components/ContactForm.js
+++ progate/React/react3&4/src/components/ContactForm.js
@@ -1,18 +1,55 @@
 import React from "react";
 
 class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isSubmitted: false, email: "", hasEmailError: false };
+  }
+  handleSubmit() {
+    this.setState({ isSubmitted: true });
+  }
+  handleEmailChange(event) {
+    const inputValue = event.target.value;
+    this.setState({ email: inputValue });
+  }
+
   render() {
-    return (
-      <div className="contact-form">
-        <form action="">
+    let emailErrorText;
+    if (this.state.hasEmailError) {
+      emailErrorText = (
+        <p className="contact-message-error">
+          メールアドレスを入力してください
+        </p>
+      );
+    }
+
+    let contactForm;
+    if (this.state.isSubmitted) {
+      contactForm = <div className="contact-submit-message">送信完了</div>;
+    } else {
+      contactForm = (
+        <form
+          action=""
+          onSubmit={() => {
+            this.handleSubmit();
+          }}
+        >
           <p>メールアドレス（必須）</p>
-          <input type="email" name="" id="" />
+          <input
+            value={this.state.email}
+            onChange={(event) => {
+              this.handleEmailChange(event);
+            }}
+          />
+          {emailErrorText}
           <p>お問い合わせ内容（必須）</p>
           <textarea name="" id="" cols="30" rows="10"></textarea>
           <input type="submit" value="送信" />
         </form>
-      </div>
-    );
+      );
+    }
+
+    return <div className="contact-form">{contactForm}</div>;
   }
 }
 
